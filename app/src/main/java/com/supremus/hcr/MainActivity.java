8e2b811c4caacb2e4c8aac6898e1d628c8250253
Jay Lal <jay.lal@somaiya.edu>
2017-09-28 14:30:11 +0530

    Integrated with OpenCV

--- app/src/main/java/com/supremus/hcr/MainActivity.java
+++ app/src/main/java/com/supremus/hcr/MainActivity.java
@@ -2,8 +2,20 @@ package com.supremus.hcr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity";
+
+    static{
+        if(OpenCVLoader.initDebug())
+            Log.d(TAG,"*********Working fine.................");
+        else
+            Log.d(TAG,"*********Not working.......");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
